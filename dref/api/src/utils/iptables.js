2beb53c21566b14f920640fa4835cf3c957395cb
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2018-07-18 19:36:53 +0100

    implement iptables util

---
+++ dref/api/src/utils/iptables.js
@@ -0,0 +1,90 @@
+import {spawn} from 'child_process'
+
+export const Command = Object.freeze({
+  APPEND: '-A',
+  CHECK: '-C',
+  DELETE: '-D',
+  INSERT: '-I'
+})
+
+export const Target = Object.freeze({
+  DROP: 'DROP',
+  REDIRECT: 'REDIRECT'
+})
+
+export const Table = Object.freeze({
+  FILTER: 'filter',
+  NAT: 'nat'
+})
+
+export const Chain = Object.freeze({
+  INPUT: 'INPUT',
+  PREROUTING: 'PREROUTING'
+})
+
+function getRule ({table, command, chain, target, fromPort, toPort, srcAddress}) {
+  fromPort = fromPort || null
+  toPort = toPort || null
+  srcAddress = srcAddress || null
+
+  let args = []
+  args = args.concat(['-t', table])
+  args = args.concat([command, chain])
+  args = args.concat(['-p', 'tcp'])
+
+  if (srcAddress) args = args.concat(['--src', srcAddress])
+  if (fromPort) args = args.concat(['--dport', fromPort])
+
+  args = args.concat(['-j', target])
+
+  if (toPort) args = args.concat(['--to-port', toPort])
+
+  return args
+}
+
+function checkRuleExists (args) {
+  // returns true if the rule with args alreadys exists
+  return new Promise((resolve, reject) => {
+    const check = spawn('iptables', args)
+
+    check.on('close', (code) => {
+      if (code === 1) return resolve(false)
+      return resolve(true)
+    })
+  })
+}
+
+export function execute ({table, command, chain, target, fromPort, toPort, srcAddress} = {}) {
+  fromPort = fromPort || null
+  toPort = toPort || null
+  srcAddress = srcAddress || null
+
+  checkRuleExists(getRule({
+    table: table,
+    command: Command.CHECK,
+    chain: chain,
+    target: target,
+    fromPort: fromPort,
+    toPort: toPort,
+    srcAddress: srcAddress
+  })).then((exists) => {
+    if (([Command.APPEND, Command.INSERT].includes(command) && exists) || (command === Command.DELETE && !exists)) {
+      return console.log(`ignoring execute(${table}, ${command}, ${chain}, ${target}, ${fromPort}, ${toPort}, ${srcAddress})`)
+    }
+
+    const iptables = spawn('iptables', getRule({
+      table: table,
+      command: command,
+      chain: chain,
+      target: target,
+      fromPort: fromPort,
+      toPort: toPort,
+      srcAddress: srcAddress
+    }))
+
+    iptables.on('close', (code) => {
+      if (code === 0) return console.log(`success execute(${table}, ${command}, ${chain}, ${target}, ${fromPort}, ${toPort}, ${srcAddress})`)
+      return console.log(`fail execute(${table}, ${command}, ${chain}, ${target}, ${fromPort}, ${toPort}, ${srcAddress})`)
+    })
+  })
+}
